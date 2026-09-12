3a4df1fd5cdd8e66d273058901eb200ab77faa02
adamz <adam@zethrae.us>
2026-09-12 10:48:33 +0100

    0.13.1

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     .library(name: "SwiftTUITerminalView", targets: ["SwiftTUITerminalView"])
   ],
   dependencies: [
-    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.13.0"),
+    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.13.1"),
     .package(url: "https://github.com/migueldeicaza/SwiftTerm.git", from: "1.18.0"),
     .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.5.0"),
   ],
